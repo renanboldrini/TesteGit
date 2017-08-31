927520e29b0b256c9e998a95affed46149aaf8ec
“Renan” <renanboldrini408@gmail.com>
2017-08-31 19:20:35 -0300

    Atualizando o projeto

--- diretorio1/TesteGit/src/testegit/TesteGit.java
+++ diretorio1/TesteGit/src/testegit/TesteGit.java
@@ -4,6 +4,6 @@ public class TesteGit {
 
 
     public static void main(String[] args) {
-        System.out.println("Testando Git ..");
+        System.out.println("Atualizando Testando Git ..");
     }    
 }
